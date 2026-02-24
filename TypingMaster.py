5be67353037ea4623d6dfbf3d5754027902161dd
Anaso2010 <anasofiaperezmartinez10@gmail.com>
2026-02-24 08:27:03 -0400

    add random word selection

--- TypingMaster.py
+++ TypingMaster.py
@@ -5,3 +5,8 @@ WORDS = [
     "peach", "melon", "kiwi", "mango", "pear",
     "plum", "lemon", "lime", "apricot", "papaya"
 ]
+
+TOTAL_WORDS = 15
+words_to_type = random.sample(WORDS, TOTAL_WORDS)
+
+print(words_to_type)
